94049c6c00d0f2cdba3fa1ea9241f01f52479b70
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-06-15 13:00:18 +0300

    Test auto CI trigger

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "DevOps Students Registry",
+      msg: "Students Registry",
       students: students
     };
     res.render('home', model);
